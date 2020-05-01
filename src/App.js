458d690c5e4075ec42c7f58e6ab854de8d6306f1
Daniel <arangomonsalve.daniel@gmail.com>
2020-05-01 17:52:51 -0500

    BancoImagenes OK

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
 
     const [busqueda, guardarBusqueda] = useState('');
     const [imagenes, guardarImagenes] = useState([]);
+    const [paginaActual, guardarPaginaActual] = useState(1);
+    const [totalPaginas, guardarTotalPaginas] = useState(1)
 
     useEffect(() => {
         if (busqueda === '') return;
@@ -14,18 +16,43 @@ function App() {
         const consultarAPI = async () => {
             const imagenesPorPagina = 30;
             const key = 'DwqDGxtVsNcf71X-KjEo_n61jtt1yeXZ92UEdRv1iOQ'
-            const url = `https://api.unsplash.com/search/photos?page=1&per_page=${imagenesPorPagina}&query=${busqueda}&client_id=${key}`
+            const url = `https://api.unsplash.com/search/photos?page=${paginaActual}&per_page=${imagenesPorPagina}&query=${busqueda}&client_id=${key}`
 
             const respuesta = await fetch(url);
             const resultado = await respuesta.json();
 
             guardarImagenes(resultado.results)
+            guardarTotalPaginas(resultado.total_pages);
+
+            //Mover la pantalla hacia arriba
+            const jumbotron = document.querySelector('.jumbotron')
+            jumbotron.scrollIntoView({behavior:'smooth'})
         }
 
         consultarAPI();
 
 
-    }, [busqueda])
+    }, [busqueda, paginaActual])
+
+    //Actualizar Pagina
+    const paginaAnterior = () => {
+        if (paginaActual !== 1) {
+            const nuevaPaginaActual = paginaActual - 1
+            guardarPaginaActual(nuevaPaginaActual)
+            console.log(nuevaPaginaActual)
+        }
+    }
+
+    const paginaSiguiente = () => {
+        if (paginaActual !== totalPaginas) {
+            const nuevaPaginaActual = paginaActual + 1
+            guardarPaginaActual(nuevaPaginaActual)
+            console.log(nuevaPaginaActual)
+        }
+    }
+
+
+
 
     return (
         <div className="container">
@@ -37,6 +64,29 @@ function App() {
             </div>
             <div className="row justify-content-center">
                 <ListadoImagenes imagenes={imagenes}/>
+                {(paginaActual === 1) ? null
+                    :
+                    <button
+                        type='button'
+                        className='btn btn-info mr-1'
+                        onClick={paginaAnterior}
+                    >&laquo; Anterior
+                    </button>
+
+
+                }
+                {(paginaActual === totalPaginas) ? null
+                    :
+
+                    <button
+                        type='button'
+                        className='btn btn-info ml-1'
+                        onClick={paginaSiguiente}
+                    >Siguiente &raquo;
+                    </button>
+
+                }
+
             </div>
 
         </div>
--- src/components/Imagen.jsx
+++ src/components/Imagen.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import style from './ContenedorImagen.module.css'
 
 const Imagen = ({imagen}) => {
 
-    const {urls, tags, likes} = imagen
+    const {urls, tags} = imagen
 
     return (
-        <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+        <div className={`col-12 col-sm-6 col-md-4 col-lg-3 mb-4`}>
             <div className="card">
-                <img src={urls.thumb} alt={tags} className='card-img-top'/>
+                <img src={urls.thumb} alt={tags} className={`${style.tamanio} card-img-top`}/>
+                <div className="card-footer">
+                    <a
+                        href={urls.full}
+                        target='_blank'
+                        rel='noreferrer noopener'
+                        className='btn btn-primary btn-block'
+                    >Ver Imagen</a>
+                </div>
             </div>
         </div>
     );

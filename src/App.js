337f9575fccae1381b824888c4dc86c5c4f9629a
Daniel <arangomonsalve.daniel@gmail.com>
2020-05-01 16:34:29 -0500

    Consultas API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Formulario from "./components/Formulario";
+import ListadoImagenes from "./components/ListadoImagenes";
 
 
 function App() {
+
+    const [busqueda, guardarBusqueda] = useState('');
+    const [imagenes, guardarImagenes] = useState([]);
+
+    useEffect(() => {
+        if (busqueda === '') return;
+
+        const consultarAPI = async () => {
+            const imagenesPorPagina = 30;
+            const key = 'DwqDGxtVsNcf71X-KjEo_n61jtt1yeXZ92UEdRv1iOQ'
+            const url = `https://api.unsplash.com/search/photos?page=1&per_page=${imagenesPorPagina}&query=${busqueda}&client_id=${key}`
+
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            guardarImagenes(resultado.results)
+        }
+
+        consultarAPI();
+
+
+    }, [busqueda])
+
     return (
         <div className="container">
             <div className="jumbotron">
                 <p className="lead text-center">Buscador de Imágenes</p>
-                <Formulario/>
+                <Formulario
+                    guardarBusqueda={guardarBusqueda}
+                />
             </div>
+            <div className="row justify-content-center">
+                <ListadoImagenes imagenes={imagenes}/>
+            </div>
+
         </div>
     );
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Error from "./Error";
 
-const Formulario = () => {
+const Formulario = ({guardarBusqueda}) => {
+
+    //Crear state
+    const [termino, guardarTermino] = useState('');
+    const [error, guardarError] = useState(false);
+
+    //Cuando el usuario busque una imagen
+    const buscarImagenes = e => {
+        e.preventDefault()
+
+        //Validar formulario
+        if (termino.trim() === '') {
+            guardarError(true);
+            return;
+        }
+
+        //Enviar busqueda al componente principal
+        guardarError(false);
+        guardarBusqueda(termino);
+
+
+    }
+
+
     return (
-        <form>
+        <form onSubmit={buscarImagenes}>
             <div className="row">
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className='form-control form-control-lg'
                         placeholder='Busca una imagen'
+                        onChange={e => guardarTermino(e.target.value)}
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -19,6 +44,7 @@ const Formulario = () => {
                     />
                 </div>
             </div>
+            {error ? <Error mensaje={'Agrega un término de búsqueda'}/> : null}
         </form>
     );
 };
